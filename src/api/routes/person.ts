a85999002591d80cfee0bd7837fc7184a090412d
AlisaLC <ali_sal1381@yahoo.com>
2022-02-18 22:49:17 +0330

    person model changes

--- src/api/routes/person.ts
+++ src/api/routes/person.ts
@@ -7,64 +7,43 @@ import { Position } from "../../domain/person";
 export function personRoutesPlugin() {
     return async (app: FastifyInstance, options: FastifyPluginOptions, done: HookHandlerDoneFunction) => {
         app.get('/', async (request, reply) => {
-            if (!request.identity || !request.identity.isAuthenticated || !request.identity.user) {
-                throw new UnauthorizedError("identity invalid!");
-            }
             const service = request.container.get<PersonApplicationService>(PersonApplicationService);
             const people = await service.getAll();
             reply.status(200).send(people);
         });
-        app.post<{ Body: { name: string, title: string, about: string, isSpeaker: boolean, photoId: number, posterId: number } }>('/', async (request, reply) => {
+        app.post<{ Body: { name: string, position: 0 | 1 } }>('/', async (request, reply) => {
             if (!request.identity || !request.identity.isAuthenticated || !request.identity.user) {
                 throw new UnauthorizedError("identity invalid!");
             }
             const service = request.container.get<PersonApplicationService>(PersonApplicationService);
-            const mediaService = request.container.get<MediaApplicationService>(MediaApplicationService);
-            let person = await service.create(request.body.isSpeaker ? Position.Speaker : Position.Staff, request.body.name);
-            person.title = request.body.title;
-            person.about = request.body.about;
-            if (request.body.photoId) {
-                const photo = await mediaService.get(request.body.photoId);
-                person.photo = photo;
-            }
-            if (request.body.posterId) {
-                const photo = await mediaService.get(request.body.posterId);
-                person.photo = photo;
-            }
+            let person = await service.create(request.body.position === 0 ? Position.Speaker : Position.Staff, request.body.name);
             person = await service.update(person);
             reply.status(200).send(person);
         });
         app.get('/speaker', async (request, reply) => {
-            if (!request.identity || !request.identity.isAuthenticated || !request.identity.user) {
-                throw new UnauthorizedError("identity invalid!");
-            }
             const service = request.container.get<PersonApplicationService>(PersonApplicationService);
             const people = await service.getAllByType(Position.Speaker);
             reply.status(200).send(people);
         });
         app.get('/staff', async (request, reply) => {
-            if (!request.identity || !request.identity.isAuthenticated || !request.identity.user) {
-                throw new UnauthorizedError("identity invalid!");
-            }
             const service = request.container.get<PersonApplicationService>(PersonApplicationService);
             const people = await service.getAllByType(Position.Staff);
             reply.status(200).send(people);
         });
         app.get<{ Params: { id: number } }>('/:id', { schema: { params: { id: { type: 'number' } } } }, async (request, reply) => {
-            if (!request.identity || !request.identity.isAuthenticated || !request.identity.user) {
-                throw new UnauthorizedError("identity invalid!");
-            }
             const service = request.container.get<PersonApplicationService>(PersonApplicationService);
             const person = await service.get(request.params.id);
             reply.status(200).send(person);
         });
-        app.put<{ Params: { id: number }, Body: { title: string, about: string, photoId: number, posterId: number } }>('/:id', { schema: { params: { id: { type: 'number' } } } }, async (request, reply) => {
+        app.put<{ Params: { id: number }, Body: { name: string, position: 0 | 1, title: string, about: string, photoId: number, posterId: number } }>('/:id', { schema: { params: { id: { type: 'number' } } } }, async (request, reply) => {
             if (!request.identity || !request.identity.isAuthenticated || !request.identity.user) {
                 throw new UnauthorizedError("identity invalid!");
             }
             const service = request.container.get<PersonApplicationService>(PersonApplicationService);
             const mediaService = request.container.get<MediaApplicationService>(MediaApplicationService);
             let person = await service.get(request.params.id);
+            if (request.body.name) person.name = request.body.name;
+            if (request.body.position) person.position = request.body.position === 1 ? Position.Staff : Position.Speaker;
             if (request.body.title) person.title = request.body.title;
             if (request.body.about) person.about = request.body.about;
             if (request.body.photoId) {
--- src/domain/person.ts
+++ src/domain/person.ts
@@ -1,5 +1,5 @@
 import { injectable } from "inversify";
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, IsNull, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Media } from './media';
 
 export enum Position {
@@ -15,18 +15,18 @@ export class Person {
     @Column()
     name: string;
 
-    @OneToOne(() => Media)
+    @OneToOne(() => Media, { nullable: true })
     @JoinColumn()
     photo: Media;
 
-    @OneToOne(() => Media)
+    @OneToOne(() => Media, { nullable: true })
     @JoinColumn()
     poster: Media;
 
-    @Column()
+    @Column({ nullable: true })
     title: string;
 
-    @Column()
+    @Column({ nullable: true })
     about: string;
 
     @Column({
