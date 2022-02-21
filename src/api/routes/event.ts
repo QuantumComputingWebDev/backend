166d797cb8183c6c40c017e2c375a263455c550f
AlisaLC <ali_sal1381@yahoo.com>
2022-02-21 06:04:16 +0330

    added date change by id

--- src/api/routes/event.ts
+++ src/api/routes/event.ts
@@ -72,7 +72,7 @@ export function eventRoutesPlugin() {
             event = await service.updateDay(event);
             reply.status(200).send(event);
         });
-        app.put<{ Params: { id: number }, Body: { posterId: number } }>('/day/:id', async (request, reply) => {
+        app.put<{ Params: { id: number }, Body: { posterId: number, date: string } }>('/day/:id', async (request, reply) => {
             if (!request.identity || !request.identity.isAuthenticated || !request.identity.user) {
                 throw new UnauthorizedError("identity invalid!");
             }
@@ -83,10 +83,13 @@ export function eventRoutesPlugin() {
                 const photo = await mediaService.get(request.body.posterId);
                 event.poster = photo;
             }
+            if (request.body.date) {
+                event.date = request.body.date;
+            }
             event = await service.updateDay(event);
             reply.status(200).send(event);
         });
-        app.put<{ Params: { id: number }, Body: { posterId: number, speakerId: number, title: string, startTime: string, endTime: string, description: string, briefDescription: string } }>('/speech/:id', async (request, reply) => {
+        app.put<{ Params: { id: number }, Body: { posterId: number, speakerId: number, title: string, startTime: string, endTime: string, description: string, briefDescription: string, date: string } }>('/speech/:id', async (request, reply) => {
             if (!request.identity || !request.identity.isAuthenticated || !request.identity.user) {
                 throw new UnauthorizedError("identity invalid!");
             }
@@ -107,6 +110,9 @@ export function eventRoutesPlugin() {
             if (request.body.endTime) event.startTime = request.body.endTime;
             if (request.body.description) event.description = request.body.description;
             if (request.body.briefDescription) event.briefDescription = request.body.briefDescription;
+            if (request.body.date) {
+                event.day = await service.getDay(new Date(request.body.date))
+            }
             event = await service.updateSpeech(event);
             reply.status(200).send(event);
         });
